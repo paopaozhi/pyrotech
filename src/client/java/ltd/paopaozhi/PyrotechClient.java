9717120b8941a385fba67378d4d9d6e04fca6bc0
paopaozhi <paopaozhi@hotmail.com>
2024-02-12 22:13:04 +0800

    init project

---
+++ src/client/java/ltd/paopaozhi/PyrotechClient.java
@@ -0,0 +1,10 @@
+package ltd.paopaozhi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PyrotechClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/ltd/paopaozhi/PyrotechDataGenerator.java
@@ -0,0 +1,11 @@
+package ltd.paopaozhi;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PyrotechDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
